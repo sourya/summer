5ba7fec90302ca8497cccf6fceaa33456be3329d
Jaipradeesh <jaipradeesh@gmail.com>
2016-04-28 07:16:31 +0530

    Added empty body check in write handler
    Signed-off-by: Jaipradeesh <jaipradeesh@gmail.com>

--- write.go
+++ write.go
@@ -20,17 +20,25 @@ func writer(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		buffer.ReadFrom(r.Body)
 		requestData := buffer.Bytes()
 
-		var requestObj map[string]string
-		err := json.Unmarshal(requestData, &requestObj)
+		if len(requestData) == 0 {
+			writeFile(w, r, path, "")
+		} else {
+			var requestObj map[string]string
+			err := json.Unmarshal(requestData, &requestObj)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
 
-		for _, value := range requestObj {
-			switch value {
-			case "content":
-				writeFile(w, r, path, requestObj["content"])
-				break
-			default:
-				errorHandler(w, r, "write", err, path)
-				break
+			for key, value := range requestObj {
+				switch key {
+				case "content":
+					writeFile(w, r, path, value)
+					break
+				default:
+					errorHandler(w, r, "write", err, path)
+					break
+				}
 			}
 		}
 	}
@@ -40,14 +48,18 @@ func writeFolder(w http.ResponseWriter, r *http.Request, path string) {
 	err := os.Mkdir(path, 0755)
 	if err != nil {
 		errorHandler(w, r, "write", err, path)
+	} else {
+		message := ResponseObj{"write", nil, time.Now(), path, ""}
+		response, err := json.Marshal(message)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
 	}
-
-	message := ResponseObj{"write", nil, time.Now(), path, []string{}}
-	response, err := json.Marshal(message)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(response)
 }
 
 func writeFile(w http.ResponseWriter, r *http.Request, path string, content string) {
@@ -55,13 +67,17 @@ func writeFile(w http.ResponseWriter, r *http.Request, path string, content stri
 	err := ioutil.WriteFile(path, contentByte, 0755)
 	if err != nil {
 		errorHandler(w, r, "write", err, path)
+	} else {
+		message := ResponseObj{"write", nil, time.Now(), path, ""}
+		response, err := json.Marshal(message)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
 	}
-
-	message := ResponseObj{"write", nil, time.Now(), path, []string{}}
-	response, err := json.Marshal(message)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(response)
 
 }
